58d70d951c286b4daa6109c1a5acfb47d2e17c04
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-08 08:09:04 +0800

    Initial Commit

---
+++ HHSuperAdmin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HHSuperAdmin
+//
+//  Created by StartupBuilder.INFO on 9/8/20.
+//  Copyright © 2020 StartupBuilder.INFO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
